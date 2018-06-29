684123943f35cf7d14d8917db385113e1030aba3
cmsnow <cmsnow@webcmsd.com>
2018-06-28 20:39:55 -0400

    First commit, basic website skeleton

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE HTML>
+<!--
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Paul Yuan</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header" class="wrapper">
+
+					<!-- Logo -->
+						<div id="logo">
+							<h1>Paul Yuan</h1>
+							<p>Bachelors in Computer Science | UC Davis 2018</p>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.html">Home</a></li>
+								<li><a href="#aboutme">About Me</a></li>
+								<li><a href="#projects">Projects</a></li>
+								<li><a href="#contact">Contact</a></li>
+							</ul>
+						</nav>
+
+				</section>
+
+			<!-- Intro -->
+				<section id="intro" class="wrapper style1">
+					<div class="title">Introduction</div>
+					<div class="container">
+						<p class="style1">So in case you were wondering what this is all about ...</p>
+						<p class="style2">
+							I'm looking for a Software Engineering Position.
+						</p>
+						<p class="style3">It's <strong>responsive</strong>, built on <strong>HTML5</strong> and <strong>CSS3</strong>, and released for
+						free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0 license</a>, so use it for any of
+						your personal or commercial projects &ndash; just be sure to credit us!</p>
+						<ul class="actions">
+							<li><a href="#aboutme" class="button style3 large">Proceed</a></li>
+						</ul>
+					</div>
+				</section>
+
+			<!-- About me section -->
+				<section id="main" class="wrapper style2">
+
+					<a name="aboutme">
+					<div class="title">About Me</div>
+					<div class="container">
+
+
+						<!-- Features -->
+							<section id="features">
+
+								<div class=mycontent>
+									<div class=mytext>
+										<p>
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+										</p>
+
+										<p>
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+										</p>
+
+										<p>
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+											My name is Paul Yuan, Im looking for stuff to do in the work thing
+										</p>
+									</div>
+
+									<div class="image featured">
+										<img src="images/me.png" alt="" />
+									</div>
+
+								</div>
+								<ul class="actions special">
+									<li><a href="#" class="button style1 large">Get Started</a></li>
+									<li><a href="#" class="button style2 large">More Info</a></li>
+								</ul>
+							</section>
+
+					</div>
+				</section>
+
+			<!-- Highlights -->
+				<section id="highlights" class="wrapper style3">
+
+
+					<div class="title">Projects</div>
+					<div class="container">
+						<div class="row aln-center">
+							<div class="col-4 col-12-medium">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+									<h3><a href="#">Photo Gallery</a></h3>
+									<p>Created a website where users could search through a database of photos</p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Learn More</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+									<h3><a href="#">User Thread Library</a></h3>
+									<p>Custom user thread library in c</p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Learn More</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section class="highlight">
+									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+									<h3><a href="#">Algorithim implementations</a></h3>
+									<p>Implemented many algorithms for practice in understanding them better. </p>
+									<ul class="actions">
+										<li><a href="#" class="button style1">Learn More</a></li>
+									</ul>
+								</section>
+							</div>
+						</div>
+
+					</div>
+
+				</section>
+
+			<!-- Footer -->
+
+				<section id="footer" class="wrapper">
+					<a name="contact"> </a>
+					<div class="title">Contact Info</div>
+					<div class="container">
+						<header class="style1">
+
+							<h2>Ipsum sapien elementum portitor?</h2>
+							<p>
+								Sed turpis tortor, tincidunt sed ornare in metus porttitor mollis nunc in aliquet.<br />
+								Nam pharetra laoreet imperdiet volutpat etiam feugiat.
+							</p>
+						</header>
+						<div class="row">
+
+
+								<!-- Contact -->
+
+									<section class="feature-list small">
+										<div class="row">
+
+											<div class="col-6 col-12-small">
+												<section>
+													<h3 class="icon fa-comment">Social</h3>
+													<p>
+														<a href="https://www.linkedin.com/in/paul-yuan-3a1435160/">linkedin.com/in/paul-yuan-3a1435160/</a><br />
+
+													</p>
+												</section>
+											</div>
+
+											<div class="col-6 col-12-small">
+												<section>
+													<h3 class="icon fa-envelope">Email</h3>
+													<p>
+														<a>paulyuan123@gmail.com</a>
+													</p>
+												</section>
+											</div>
+
+											<div class="col-6 col-12-small">
+												<section>
+													<h3 class="icon fa-phone">Phone</h3>
+													<p>
+														(530) 941-5320
+													</p>
+												</section>
+											</div>
+
+										</div>
+									</section>
+
+						</div>
+						<div id="copyright">
+							<ul>
+								<li>&copy; Paul Yuan.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</div>
+				</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
